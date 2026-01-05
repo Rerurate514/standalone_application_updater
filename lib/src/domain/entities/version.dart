1170d6381e204d5b502a42a7f807f8a34a964ad8
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 16:25:00 +0900

    正規化追加

--- lib/src/domain/entities/version.dart
+++ lib/src/domain/entities/version.dart
@@ -4,7 +4,21 @@ part 'version.freezed.dart';
 
 @freezed
 sealed class Version with _$Version {
+  const Version._();
+
   const factory Version({
     required String value
   }) = _Version;
+
+  String get normalized => value.replaceFirst('v', '').split('+')[0];
+
+  @override
+  bool operator ==(Object other) {
+    if (identical(this, other)) return true;
+    if (other is! Version) return false;
+    return normalized == other.normalized;
+  }
+
+  @override
+  int get hashCode => normalized.hashCode;
 }
