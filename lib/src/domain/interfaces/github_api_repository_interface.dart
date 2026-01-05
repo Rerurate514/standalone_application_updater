f27b87d8aa9e0d7b7bbf45aa0b4a2add516f10f6
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 15:43:54 +0900

    名称の変更

--- lib/src/domain/interfaces/github_api_repository_interface.dart
+++ lib/src/domain/interfaces/github_api_repository_interface.dart
@@ -1,6 +1,6 @@
 import 'package:standalone_application_updater/src/domain/entities/repository_info.dart';
 import 'package:standalone_application_updater/src/infrastructure/models/github_release_response.dart';
 
-abstract class GithubApiRepositoryInterface {
+abstract class IGithubApiRepository {
   Future<GithubReleaseResponse?> fetchLatestRelease(RepositoryInfo info);
 }
