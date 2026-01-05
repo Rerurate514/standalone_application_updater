288e1121e6c5ad1fbc1a5a38cb122d9d5ad53e25
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 16:20:27 +0900

    config追加

---
+++ lib/src/domain/entities/sau_config.dart
@@ -0,0 +1,8 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'sau_config.freezed.dart';
+
+@freezed
+class SAUConfig with _$SAUConfig {
+  factory SAUConfig() = _SAUConfig;
+}
