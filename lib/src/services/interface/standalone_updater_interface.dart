c7b0cbda67aac1e5454f7a3a48e9bcd1fc4dba9f
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 17:37:45 +0900

    名称の変更

--- lib/src/services/interface/standalone_updater_interface.dart
+++ lib/src/services/interface/standalone_updater_interface.dart
@@ -1,7 +1,7 @@
 import 'package:standalone_application_updater/src/domain/entities/repository_info.dart';
 import 'package:standalone_application_updater/src/domain/entities/sau_config.dart';
 
-abstract class IStandaloneUpdateService {
+abstract class IStandaloneUpdateBase {
   Future<bool> checkForUpdates(RepositoryInfo repoInfo, SAUConfig config);
   Future<void> downloadUpdate();
   Future<void> applyUpdate();
--- lib/src/services/standalone_update_service.dart
+++ lib/src/services/standalone_update_service.dart
@@ -3,11 +3,11 @@ import 'package:standalone_application_updater/src/domain/entities/repository_in
 import 'package:standalone_application_updater/src/domain/entities/sau_config.dart';
 import 'package:standalone_application_updater/src/infrastructure/repositories/github_api_repository.dart';
 import 'package:standalone_application_updater/src/infrastructure/repositories/package_info_repository.dart';
-import 'package:standalone_application_updater/src/services/standalone_updater_interface.dart';
+import 'package:standalone_application_updater/src/services/interface/standalone_updater_interface.dart';
 import 'package:standalone_application_updater/src/services/update_check_service.dart';
 import 'package:standalone_application_updater/src/utils/my_logger.dart';
 
-class StandaloneUpdateService extends IStandaloneUpdateService with MyLogger {
+class StandaloneUpdateService extends IStandaloneUpdateBase with MyLogger {
   @override
   Future<bool> checkForUpdates(RepositoryInfo repoInfo, SAUConfig config) async {
     final ucs = UpdateCheckService(
