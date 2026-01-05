8b1e0c0938e1b91f09af74cd3e85055e642d03f0
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 12:56:54 +0900

    gar追加

---
+++ lib/src/infrastructure/repositories/github_api_repository.dart
@@ -0,0 +1,48 @@
+import 'package:dio/dio.dart';
+import 'package:standalone_application_updater/src/domain/entities/repository_info.dart';
+import 'package:standalone_application_updater/src/domain/interfaces/github_api_repository_interface.dart';
+import 'package:standalone_application_updater/src/infrastructure/models/github_release_response.dart';
+import 'package:standalone_application_updater/src/utils/constants.dart';
+import 'package:standalone_application_updater/src/utils/my_logger.dart';
+
+class GithubApiRepositoryImpl extends GithubApiRepositoryInterface with MyLogger {
+  final Dio dio;
+
+  GithubApiRepositoryImpl({
+    required this.dio,
+  });
+
+  @override
+  Future<GithubReleaseResponse?> fetchLatestRelease(RepositoryInfo info) async {
+    final String url = '${Constants.baseUrl}/repos/${info.owner}/${info.repoName}/releases/latest';
+    final response = await _executeRequest(url);
+    
+    if (response == null || response.data == null) return null;
+
+    try {
+      final release = GithubReleaseResponse.fromJson(response.data as Map<String, dynamic>);
+      return release;
+    } catch (e) {
+      error('Deserialization Error: $e');
+      return null;
+    }
+  }
+
+  Future<Response?> _executeRequest(String url) async {
+    try {
+      final response = await dio.get(
+        url,
+        options: Options(
+          headers: {
+            'Accept': 'application/vnd.github.v3+json',
+          },
+        ),
+      );
+
+      return response;
+    } catch (e) {
+      error('API Request Error: $e');
+      return null;
+    }
+  }
+}
