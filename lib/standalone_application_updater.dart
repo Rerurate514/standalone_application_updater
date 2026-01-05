0907815ff53de3a0bd6f99e6f022ff431b0c4749
Rerurate514 <rerurateyuto@gmail.com>
2026-01-05 17:21:10 +0900

    export設定

--- lib/standalone_application_updater.dart
+++ lib/standalone_application_updater.dart
@@ -1,5 +1,3 @@
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'src/domain/entities/sau_config.dart';
+export 'src/domain/entities/repository_info.dart';
+export 'src/services/standalone_update_service.dart';
