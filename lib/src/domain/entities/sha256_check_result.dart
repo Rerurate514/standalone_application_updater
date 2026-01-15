65f0ce7023dce9fa448ca8ce94fdce0a389b7497
Rerurate514 <rerurateyuto@gmail.com>
2026-01-16 04:31:50 +0900

    interfaceの名称変更

--- lib/src/domain/entities/sha256_check_result.dart
+++ lib/src/domain/entities/sha256_check_result.dart
@@ -2,13 +2,13 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 
 part 'sha256_check_result.freezed.dart';
 
-abstract interface class IDownloadSuccess {}
+abstract interface class ISha256DownloadSuccess {}
 
 @freezed
 sealed class Sha256CheckResult with _$Sha256CheckResult {
-  @Implements<IDownloadSuccess>()
+  @Implements<ISha256DownloadSuccess>()
   factory Sha256CheckResult.valid() = Sha256CheckValid;
-  @Implements<IDownloadSuccess>()
+  @Implements<ISha256DownloadSuccess>()
   factory Sha256CheckResult.invalid() = Sha256CheckInvalid;
 
   factory Sha256CheckResult.notExist() = Sha256CheckNotExist;
